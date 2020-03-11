6941eb8fb0ea1c0948b7cab4e8d0993bede79469
jvrocha10 <60831755+jvrocha10@users.noreply.github.com>
2020-03-10 23:34:45 -0300

    Create Counting.py

---
+++ CountingSort/Counting.py
@@ -0,0 +1,81 @@
+import timeit
+
+from random import randint, shuffle
+
+import matplotlib.pyplot as plt
+
+
+
+def desenhaGrafico(x,lista1,xl = "Entradas", yl = "Y",name="out", label1 = "Lista 1", label2 = "Lista 2"):
+
+    fig = plt.figure(figsize=(12, 10))
+
+    ax = fig.add_subplot(111)
+
+    ax.plot(x, lista1, label = label1)
+
+    ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
+
+    plt.ylabel(yl)
+
+    plt.xlabel(xl)
+
+    plt.savefig(name)
+
+
+
+def geraLista(tam):
+
+    lista = list(range(1, tam + 1))
+
+    shuffle(lista)
+
+    return lista
+
+
+
+def sort(lista, maximo):
+
+    count(lista, maximo)
+
+
+
+def count(lista, maximo):
+
+	count = [0] * (maximo+1)
+
+	listaOrdenada = [None] * len(lista)
+
+	for i in range(len(lista)):
+
+		count[lista[i]] += 1
+
+	for i in range(1, maximo+1):
+
+		count[i] += count[i-1] 
+
+	for i in range(len(lista)):
+
+		listaOrdenada[count[lista[i]]-1] = lista[i]
+
+		count[lista[i]] -= 1
+
+
+
+quant = [30000,40000,50000,60000,70000]
+
+graf_tempo = []
+
+
+
+for i in range(len(quant)):
+
+    print(quant[i])
+
+    lista = geraLista(quant[i])
+
+    graf_tempo.append(timeit.timeit("sort({},{})".format(lista, quant[i]),setup="from __main__ import sort",number=1))
+
+
+
+desenhaGrafico(quant,graf_tempo,"Tamanho", "Tempo", "Tempo", label1 = "Lista")
